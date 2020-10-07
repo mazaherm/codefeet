1e219be9799a7a31f8f4656148572256afe56dbf
Mazaher Muraj <mmuraj.313@gmail.com>
2020-10-07 13:34:47 +0100

    builds basic navbar component

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,12 +15,33 @@ module.exports = {
     `gatsby-plugin-styled-components`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Montserrat`,
+            variants: [`400`, `600`]
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        start_url: `/`,
+        icon: `src/images/codefeet-logo.png`,
+      },
+    },
     {
       resolve: `gatsby-plugin-alias-imports`,
       options: {
         alias: {
           '@src': 'src',
           '@components': 'src/components',
+          '@atomic': 'src/components/atomic',
+          '@styles': 'src/styles',
           '@hooks': 'src/hooks',
           '@pages': 'src/pages',
           '@templates': 'src/templates',
