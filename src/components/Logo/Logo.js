670c3133d43e9a834621a825df9f0af1f1c71534
Mazaher Muraj <mmuraj.313@gmail.com>
2020-10-14 00:42:02 +0100

    updates logo component

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,19 +1,42 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import styled from 'styled-components';
 import { useSiteMetadata } from '@hooks/useSiteMetadata';
 
 import Image from '@components/atomic/atoms/Image';
-import codefeetLogo from '@images/codefeet-logo.png';
+import codefeetLogo from '@images/cf.svg';
+
+const LogoBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-end;
+`;
+
+const LogoBoxText = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 1em;
+
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
 
 const Logo = () => {
-  const { title } = useSiteMetadata();
+  const { title, description } = useSiteMetadata();
   return (
-    <Link to='/'>
-      <Image
-        src={codefeetLogo}
-        alt={title}
-        className='logo'
-      />
+    <Link to='/' className='logo-link'>
+      <LogoBox>
+        <Image
+          src={codefeetLogo}
+          alt={title}
+          className='logo'
+        />
+        <LogoBoxText>
+          <h1 className='logo-title'>{title}</h1>
+          <p className='logo-desc'>{description}</p>
+        </LogoBoxText>
+      </LogoBox>
     </Link>
   )
 }
