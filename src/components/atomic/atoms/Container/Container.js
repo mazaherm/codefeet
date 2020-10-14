2b4be30393c2d5744bf1e3fd9f3d467c1aa815fb
Farshad Shafii <farshad.shafii@gmail.com>
2020-10-14 20:08:06 +0100

    creates atom container

---
+++ src/components/atomic/atoms/Container/Container.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Container = ({ 
+	children,
+	className,
+ }) => {
+	return (
+		<div className={className}>
+			{children}
+		</div>
+	)
+};
+
+export default Container;
