f833df2334fad55af5faab690fea2d618c15b32b
Farshad Shafii <farshad.shafii@gmail.com>
2020-10-14 21:04:09 +0100

    uses bootstrap components to create basic footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,15 @@
-import React from 'react'; 
-import Container from '@atomic/atoms/Container';
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const Footer = () => {
     return(
-			<Container className="footer-container">
-				Hello World
+			<Container>
+				<Row>
+					<Col>Column 1</Col>
+					<Col>Column 2</Col>
+				</Row>
 			</Container>
     );
 }
