b6b705b2b67f665a8977f06664ad35671412c10a
Mazaher Muraj <mmuraj.313@gmail.com>
2020-11-01 18:24:11 +0000

    fixes nav bar styles

--- src/components/Navbar/NavbarLinks.js
+++ src/components/Navbar/NavbarLinks.js
@@ -4,16 +4,19 @@ import { Link } from "gatsby";
 
 const StyledLink = styled((props) => <Link {...props} />)`
   margin: 0 0.5em;
-  color: $text-primary;
-  font-size: 1.125em;
+  ${({ theme }) => `color: ${theme.palette.text.primary}`};
+  font-size: 1em;
   font-weight: 600;
   text-decoration: none;
   padding: 0.5em 0.25em;
 
   &:hover {
+    ${({ theme }) => `
+      background-color: ${theme.palette.text.secondary};
+      color: ${theme.palette.text.tertiary} !important;
+    `};
     border-radius: 0.25em;
-    color: #f5f6fa;
-    background-color: #5a78f1;
+    text-decoration: none;
   }
   &:visited {
     color: inherit;
