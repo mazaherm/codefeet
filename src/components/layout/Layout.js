880ec588375ee30c8e6cd8d60a42f8d158e36449
Farshad Shafii <farshad.shafii@gmail.com>
2020-11-12 12:28:00 +0000

    added responsive design

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -11,6 +11,9 @@ import "@styles/main.scss";
 const AppStyle = styled.main`
   width: 800px;
   margin: 0 auto;
+  @media (max-width: 500px) {
+    width: 300px;
+  }
 `;
 
 const Layout = ({ children }) => {
