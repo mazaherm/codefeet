5842518adf5c51e712a8012c1f808ba69298f70d
Mazaher Muraj <mmuraj.313@gmail.com>
2020-12-19 11:35:16 +0000

    completes about page contents

--- src/config/constants.js
+++ src/config/constants.js
@@ -8,3 +8,11 @@ export const mainFooterLinks = [
   { to: "/all-posts", text: "All Posts" },
   { to: "/about", text: "About" },
 ];
+
+export const myTechStack = [
+  { item: "HTML5/CSS3" },
+  { item: "Sometimes just plain vanilla JS" },
+  { item: "React and React Native" },
+  { item: "Styled Components" },
+  { item: "TypeScript" },
+];
