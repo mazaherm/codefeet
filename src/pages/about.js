94967c4f9f5d04c9080255a4588c79797d383cc0
Mazaher Muraj <mmuraj.313@gmail.com>
2020-12-19 16:38:03 +0000

    change layout import

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,7 @@
 import React from "react";
 import SEO from "react-seo-component";
 import styled from "styled-components";
-import Layout from "src/components/Layout";
+import Layout from "./components/Layout";
 import { useSiteMetadata } from "@hooks/useSiteMetadata";
 
 import Heading from "@atomic/atoms/Heading";
