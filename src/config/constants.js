ff9088acc79362be2a787a7eaf5ac879f42b6e3d
Mazaher Muraj <mmuraj.313@gmail.com>
2020-12-19 12:41:06 +0000

    removes newsletter page and adds substack subscribe link

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,10 +1,11 @@
 export const mainNavLinks = [
   { to: "/about", text: "About" },
+  { to: "https://codefeet.substack.com/subscribe", text: "Newsletter" },
   { to: "/all-posts", text: "All Posts" },
 ];
 
 export const mainFooterLinks = [
-  { to: "/newsletter", text: "Newsletter" },
+  { to: "https://codefeet.substack.com/subscribe", text: "Newsletter" },
   { to: "/all-posts", text: "All Posts" },
   { to: "/about", text: "About" },
 ];
